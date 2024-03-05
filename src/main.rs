daa771a21dd1e3360c21e4e3e6cae0bdd290e72c
RehJunior <samet.30101468@gmail.com>
2024-03-05 11:26:15 +0100

    new from the video

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
 fn main() {
-    println!("Hello, world!");
+    let subject = "World";
+    let  greeting = format!("Hello, {}!", subject);
+    println!("{}, {}!", greeting, subject);
+     let crash_reason = "Server wanted a nap.";
+    panic!("I crashed! {}", crash_reason);
+    println!("This will never get run."); 
 }
