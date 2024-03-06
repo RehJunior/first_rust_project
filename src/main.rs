afef11a816a193fb85ed5bb34f51fe0a32890e18
RehJunior <samet.30101468@gmail.com>
2024-03-06 10:23:07 +0100

    new ways to deal with functions

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,16 @@
 fn main() {
     let subject = "World";
-    let  greeting = format!("Hello, {}!", subject);
+    let greeting = format!("Hello, {}!", subject);
     println!("{}, {}!", greeting, subject);
-     let crash_reason = "Server wanted a nap.";
+    let crash_reason = "Server wanted a nap.";
     println!("This will never get run.");
+    floats();
     panic!("I crashed! {}", crash_reason);
+}
+#[allow(dead_code)]
+fn floats() {
+    let x = 1.1;
+    let y = 2.2;
+
+    println!("x times y is {}", x * y)
 }
